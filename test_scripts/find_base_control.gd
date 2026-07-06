95700d063fa306f8d36fbe82ec3b42f87904bcc3
Kai Xu <kenyon1977@gmail.com>
2026-07-06 20:03:06 +0800

    chore: ignore top-level scripts/plans/test_scripts/tools (dev-only)

--- test_scripts/find_base_control.gd
+++
@@ -1,7 +0,0 @@
-class_name FindBaseControl
-extends EditorPlugin
-
-func get_output_panel() -> Control:
-	return EditorInterface.get_base_control().find_child("AnimationPlayerEditor", true, false)
-
-
--- test_scripts/test_script_scene.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends Node2D
-
-func _enter_tree() -> void:
-	print("===enter tree===")
-	var tool = FindBaseControl.new()
-	var panel = tool.get_output_panel()
-	print(panel)
-	
